657f42a084d09ffd6d58c915969335f7d5b32775
walra356 <walra356@planet.nl>
2024-11-18 00:48:10 +0100

    update grid.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MIT
 
-#using CamiDiff
+using CamiDiff
 using CamiMath
 
 # using IntervalSets
@@ -10,9 +10,9 @@ using Test
 
 @testset "CamiXon.jl" begin 
 
-    grid2 = castGrid1(2, 4, Float64; h = 0.1, r0 = 2.0, p=1);
-    grid3 = castGrid1(3, 4, Float64; h = 0.1, r0 = 2.0);
-    grid4 = castGrid1(4, 4, Float64; h = 0.1, r0 = 2.0, polynom=[0, 1]);
+    grid2 = castGrid(2, 4, Float64; h = 0.1, r0 = 2.0, p=1);
+    grid3 = castGrid(3, 4, Float64; h = 0.1, r0 = 2.0);
+    grid4 = castGrid(4, 4, Float64; h = 0.1, r0 = 2.0, polynom=[0, 1]);
     @test [grid2.r, grid2.r′, grid2.r′′] ≈ [grid3.r, grid3.r′, grid3.r′′] ≈ [grid4.r, grid4.r′, grid4.r′′]
     
 end
