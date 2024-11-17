2d895f6bf77d4df83b2e1e271b3b75f76dc255a5
walra356 <walra356@planet.nl>
2024-11-18 00:39:16 +0100

    work on runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,5 +9,10 @@ using LinearAlgebra
 using Test
 
 @testset "CamiXon.jl" begin 
+
+    grid2 = castGrid1(2, 4, Float64; h = 0.1, r0 = 2.0, p=1);
+    grid3 = castGrid1(3, 4, Float64; h = 0.1, r0 = 2.0);
+    grid4 = castGrid1(4, 4, Float64; h = 0.1, r0 = 2.0, polynom=[0, 1]);
+    @test [grid2.r, grid2.r′, grid2.r′′] ≈ [grid3.r, grid3.r′, grid3.r′′] ≈ [grid4.r, grid4.r′, grid4.r′′]
     
 end
