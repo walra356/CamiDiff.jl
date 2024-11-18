70dc684bab8b55c0d708d38f189e2a57cc44d5b3
walra356 <walra356@planet.nl>
2024-11-18 01:09:43 +0100

    work on runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,12 @@
 using CamiDiff
 using CamiMath
 
-# using IntervalSets
-#using BenchmarkTools
+using BenchmarkTools
 using LinearAlgebra
 using Test
 
-@testset "CamiXon.jl" begin 
-
+@testset "CamiDiff.jl" begin 
+  
     grid2 = castGrid(2, 4, Float64; h = 0.1, r0 = 2.0, p=1);
     grid3 = castGrid(3, 4, Float64; h = 0.1, r0 = 2.0);
     grid4 = castGrid(4, 4, Float64; h = 0.1, r0 = 2.0, polynom=[0, 1]);
