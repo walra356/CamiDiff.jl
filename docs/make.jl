1f672fed40668cfa71999f1ca970a3aaeacfabc9
walra356 <walra356@planet.nl>
2025-01-20 13:37:37 +0100

    wwork on docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter
 using CamiDiff
 
 makedocs(;
-    modules=[CamiDiff],
+    modules=[CamiDiff, Camimath],
     authors="= <walra356@planet.nl> and contributors",
     sitename="CamiDiff.jl",
     format=Documenter.HTML(;
