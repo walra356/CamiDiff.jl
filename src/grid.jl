f6145e3357769c6424eb811f66e8d5b2ee52a766
walra356 <walra356@planet.nl>
2025-02-01 16:42:55 +0100

    work on runtest

--- src/grid.jl
+++ src/grid.jl
@@ -471,8 +471,8 @@ end
 @doc raw"""
     grid_differentiation(f::Vector{T}, grid::Grid{T}; k=5) where T<:real
 
-``k^{th}``-order lagrangian *differentiation* of the function ``f(r)``,
-tabulated in forward order on a [`Grid`](@ref) of ``N`` points, ``f[1:N]``.
+``k^{th}``-order lagrangian *derivative* of the function ``f(r)``,
+* `f[1:N]` : the function `f(r)` tabulated in forward order on a [`Grid`](@ref) of ``N`` points.
 #### Example:
 ```
 julia> grid = castGrid(3, 1001, Float64; h=2π/1000.0, r0=1.0, msg=false);
@@ -484,10 +484,10 @@ julia> f′ = [cos(grid.r[i]) for i=1:grid.N]
 julia> grid_differentiation(f, grid) ≈ f′
 true
 ```
-    grid_differentiation(f::Vector{T}, grid::Grid{T}, rv::T, notation=fwd; k=5) where T<:Real
+    grid_differentiation(f::Vector{T}, grid::Grid{T}, r::T, notation=fwd; k=5) where T<:Real
 
-``k^{th}``-order lagrangian *derivative* ``f^′(rv)`` of the function ``f(r)``, tabulated 
-in forward order on a [`Grid`](@ref) of ``N`` points, ``f[1:N]``. 
+``k^{th}``-order lagrangian *derivative* of the function ``f(r)`` at position `r`. 
+* `f[1:N]` : the function `f(r)` tabulated in forward order on a [`Grid`](@ref) of ``N`` points.
 * `fwd` using fwd-difference notation  
 * `bwd` using bwd-difference notation
 
@@ -502,8 +502,10 @@ true
 ```
     grid_differentiation(f::Vector{T}, grid::Grid{T}, n::Int, notation=fwd; k=5) where T<:Real
 
-``k^{th}``-order lagrangian *derivative* ``f^′[n]`` of the regular function ``f(r)``, tabulated 
-in forward order on a [`Grid`](@ref) of ``N`` points, ``f[1:N]``. 
+``k^{th}``-order lagrangian *derivative* of the function ``f(r)`` at grid position `n`. 
+* `f[1:N]` : the function `f(r)` tabulated in forward order on a [`Grid`](@ref) of ``N`` points.
+* `fwd` using fwd-difference notation  
+* `bwd` using bwd-difference notation
 
 #### Example:
 ```
@@ -516,12 +518,11 @@ julia> f′ = [cos(grid.r[i]) for i=1:grid.N]
 julia> grid_differentiation(f, grid, 500, fwd) ≈ f′[500]
 true
 ```
-
     grid_differentiation(f::Vector{T}, grid::Grid{T}, n1::Int, n2::Int; k=5) where T<:Real
     grid_differentiation(f::Vector{T}, grid::Grid{T}, itr::UnitRange; k=5) where T<:Real
 
-``k^{th}``-order lagrangian *derivative* ``f′[n1:n2]`` of the regular function ``f(r)``, tabulated 
-in forward order on a [`Grid`](@ref) of ``N`` points, ``f[1:N]``.
+``k^{th}``-order lagrangian *derivative* of the function ``f(r)`` over the grid range `n1 ≤ n ≤ n2`.
+* `f[1:N]` : the function `f(r)` tabulated in forward order on a [`Grid`](@ref) of ``N`` points.
 * `n1=itr.start`, `n2=itr.stop`.  
 
 
